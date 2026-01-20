bd30a74fb5ffc7c90d1081720f750ffee0ea1410
João Gabriel <joao.gabriel.xp2011@gmail.com>
2026-01-20 19:41:16 -0300

    third commit

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
 const canvas = document.getElementById("canvas");
 const cxt = canvas.getContext("2d");
+const groundY = 440;
 
 let player = {x:260,y:370,w:40,h:70,vy:0,jump:false};
 
-let hordeArea = 100;
+let horde = {x: 0, y: 140, w: 120, h: 300};
 
 let obstacle = {x: 490,y:405,w:50,h:35,vx:-10};
 let obVel1 = 4;
-let gravity = 1.2;
-let jumpPower = -5;
+let gravity = 0.6;
+let jumpPower = -12;
+let misplaced = false;
+let collided = false;
 
 function draw(){
     cxt.clearRect(0, 0, canvas.width, canvas.height);
@@ -28,23 +31,57 @@ function draw(){
 
 function update() {
     player.y += player.vy;
-    if (player.y + player.h < 300){
+    if (player.y + player.h < groundY){
         player.vy += gravity;
     }
     else {
-        player.y = 380;
-        /*player.vy = 0;
-        player.jump=false;*/
+        player.y = groundY - player.h;
+        player.vy = 0;
+        player.jump=false;
     }
 
     obstacle.x -= obVel1;
     if(obstacle.x < -30 ){
         obstacle.x = 820;
-        if(obVel1<25){
+        if(obVel1<20){
             obVel1 += 0.5;
             console.log(obVel1);
+            console.log(collided);
         }
     }
+
+    if(
+        player.x < obstacle.x + obstacle.w &&
+        player.x + player.w > obstacle.x &&
+        player.y < obstacle.y + obstacle.h &&
+        player.y + player.h > obstacle.y
+    ){
+        if(player.y != obstacle.y)player.x -= 5;
+        collided = true;
+        player.x -= obVel1;
+    }
+
+    if(player.x < 260 && collided == false) misplaced = true;
+
+    if(misplaced){
+        player.x += 1;
+        console.log(misplaced);
+    }
+
+
+
+    /*if(
+        player.x < horde.x + horde.w &&
+        player.x + player.w > horde.x
+    ) {
+        alert("Os zumbis te derrotaram!🧟");
+        /*player.w = 70;
+        player.h = 40;
+        player.x += 30;*/
+        /*player.x = 260
+        obVel1 = 20;
+        obstacle.x - canvas.width;
+    }*/
 
     draw();
     requestAnimationFrame(update);
